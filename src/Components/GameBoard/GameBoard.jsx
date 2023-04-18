46fd2518d93708411ae1095bee22f400f7607c2f
Noah Beasley <njbeasl@corp.shemplabs.dev>
2023-04-17 20:52:53 -0400

    Add style/spelling fixes

--- src/Components/GameBoard/GameBoard.jsx
+++ src/Components/GameBoard/GameBoard.jsx
@@ -52,7 +52,7 @@ const GameBoard = () => {
               <PlayerActions />
             ):
             <p>
-              It is not Your turn yet
+              It is not your turn yet!
             </p>
           }
         
